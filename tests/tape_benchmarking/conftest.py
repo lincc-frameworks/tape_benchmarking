dc533b39cbb54e7b12df2e1f040426d93c61c98b
Doug Branton <brantd@uw.edu>
2023-06-14 12:37:04 -0700

    test file paths

--- tests/tape_benchmarking/conftest.py
+++ tests/tape_benchmarking/conftest.py
@@ -21,8 +21,8 @@ def s82_rrl_ensemble(dask_client):
     ens = Ensemble(client=dask_client)
 
     colmap = ColumnMapper(id_col="#id", time_col="mjd", flux_col="flux", err_col="error", band_col="band")
-    ens.from_parquet(source_file="../../data/s82_rrlyrae/s82rrl_source.parquet",
-                     object_file="../../data/s82_rrlyrae/s82rrl_object.parquet",
+    ens.from_parquet(source_file="data/s82_rrlyrae/s82rrl_source.parquet",
+                     object_file="data/s82_rrlyrae/s82rrl_object.parquet",
                      column_mapper=colmap,
                      provenance_label="sesar_2010")
 
@@ -36,8 +36,8 @@ def s82_qso_ensemble(dask_client):
     ens = Ensemble(client=dask_client)
 
     colmap = ColumnMapper(id_col="dbID", time_col="mjd", flux_col="flux", err_col="error", band_col="band")
-    ens.from_parquet(source_file="../../data/s82_qso/s82qso_source.parquet",
-                     object_file="../../data/s82_qso/s82qso_object.parquet",
+    ens.from_parquet(source_file="data/s82_qso/s82qso_source.parquet",
+                     object_file="data/s82_qso/s82qso_object.parquet",
                      column_mapper=colmap,
                      provenance_label="Southern Sample")
 
